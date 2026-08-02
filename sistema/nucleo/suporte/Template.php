2bbbf3413aa80f5f6c7b4ac3d381de98fa741f63
Allan César <allancbm98@gmail.com>
2026-08-02 12:55:30 -0300

    Completando anotações sobre Mensagens Flash

--- sistema/nucleo/suporte/Template.php
+++ sistema/nucleo/suporte/Template.php
@@ -32,7 +32,7 @@ class Template
                 new \Twig\TwigFunction('url', function (?string $url = null) {
                     return Helpers::url($url);
                 })
-            ),
+            ), // qualquer view que tenha essa função, ela sempre será chamda, se tiver o objeto Mensagem, vai renderizar, senão vai ser a view sem nenhuma mensagem flash
              $this->twig->addFunction(
                 new \Twig\TwigFunction('flash', function () {
                     return Helpers::flash();
