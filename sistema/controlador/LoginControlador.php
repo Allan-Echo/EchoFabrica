f5115ebe97d1511d8a2b5a8d4d90a5efc62e3079
Allan César <allancbm98@gmail.com>
2026-08-03 09:32:22 -0300

    mensagem flash

--- sistema/controlador/LoginControlador.php
+++ sistema/controlador/LoginControlador.php
@@ -15,7 +15,9 @@ class LoginControlador extends Controlador
     {
         $dados = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
         if (!empty($dados)) {
-            if 
+            if (in_array('',$dados, true)) {
+                $this->mensagem->alerta('Preencha todos os campos para efetuar o login')->flash();
+            }
         }
         echo $this->template->rendenrizar('login.html',
         [
