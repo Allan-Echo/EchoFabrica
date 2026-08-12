2602db48f600a6d4d8d4d19c6e813fb582cb9f8e
Allan César <allancbm98@gmail.com>
2026-08-12 09:09:27 -0300

    Super Class Modelo
    Criando encadeamento de funções para formar consultas sql

--- sistema/nucleo/Modelo.php
+++ sistema/nucleo/Modelo.php
@@ -65,6 +65,54 @@ abstract class Modelo
         return $this->conection->select($query, $this->parametros ?? null);
     }
 
+    protected function cadastrar(array $dados): void {
+        $dados = $this->filtro($dados);
+
+        $colunas = implode(', ', array_keys($dados));
+        $valores = ':' . implode(', :', array_keys($dados));
+        $query = "INSERT INTO {$this->tabela} ({$colunas}) VALUES ({$valores})";
+        
+        $this->conection->insert($query, $dados);
+    }
+
+    private function filtro (array $dados): array
+    {
+        $dadosFiltrados = [];
+        foreach ($dados as $key => $value) {
+            if (is_string($value)) {
+                $dadosFiltrados[$key] = trim($value);
+            } elseif (is_int($value)) {
+                $dadosFiltrados[$key] = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+            } elseif (is_float($value)) {
+                $dadosFiltrados[$key] = filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            } else {
+                $dadosFiltrados[$key] = $value;
+            }
+        }
+
+        return $dadosFiltrados;
+    }
+    /**
+     * @param array $dados um array associativo contendo os dados a serem atualizados, onde as chaves são os nomes das colunas e os valores são os novos valores a serem atribuídos.
+     * @param string $where uma string representando a cláusula WHERE da consulta SQL, que define a linha ou linhas a serem atualizadas. Por exemplo, "id = :id" para atualizar uma linha específica com base no ID.
+     * @param array $parametros um array associativo contendo os parâmetros a serem vinculados à cláusula WHERE da consulta SQL. Por exemplo, se a cláusula WHERE for "id = :id", o array de parâmetros deve conter ['id' => $valorId].
+     * @return void
+     * @throws \PDOException se ocorrer um erro durante a execução da consulta SQL.
+     */
+    protected function atualizar(array $dados, string $where, array $parametros): void 
+    {
+        $dados = $this->filtro($dados);
+
+        $set = [];
+        foreach ($dados as $key => $value) {
+            $set[] = "{$key} = :{$key}";
+        }
+        $setString = implode(', ', $set);
+        $query = "UPDATE {$this->tabela} SET {$setString} WHERE {$where}";
+
+        $this->conection->update($query, array_merge($dados, $parametros));
+    }
+
     public function buscarPorId(int $id): ?object
     {
         $query = "SELECT * FROM " . $this->tabela . " WHERE id = :id LIMIT 1";
