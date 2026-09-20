c12ec5366c34cd3bc86ec867b3e6071dccbc950b
Echo <allanecho98@outlook.com>
2026-09-20 16:05:53 -0300

    Adicionado return depois de redirecionar

--- sistema/nucleo/TratadorExcecao.php
+++ sistema/nucleo/TratadorExcecao.php
@@ -33,7 +33,7 @@ class TratadorExcecao
         if ($excecao instanceof \Pecee\SimpleRouter\Exceptions\NotFoundHttpException) {
             http_response_code(404);
             if ($this->debug) {
-                (new Sessao())->criar('erro_debug', [
+                (new Sessao())->criar('erroDebug', [
                     'mensagem' => $excecao->getMessage(),
                     'arquivo'  => $excecao->getFile(),
                     'linha'    => $excecao->getLine(),
